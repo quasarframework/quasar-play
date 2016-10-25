892b863081a332212efa09350fd1c5ad4c3934d1
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2016-10-25 12:44:52 +0300

    chore: Move Quasar CSS import to App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,15 @@
     <router-view></router-view>
   </div>
 </template>
+
+<script>
+// === DEFAULT / CUSTOM STYLE ===
+// WARNING! always comment out ONE of the two require() calls below.
+// 1. use next line to activate CUSTOM STYLE (./src/themes)
+// require('./themes/app.' + __THEME + '.styl')
+// 2. or, use next line to activate DEFAULT QUASAR STYLE
+require('quasar/dist/quasar.' + __THEME + '.css')
+// ==============================
+
+export default {}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,8 @@
 import Vue from 'vue'
 import Quasar from 'quasar'
 
-import Router from './router'
+import router from './router'
 import App from './App'
-
-// === DEFAULT / CUSTOM STYLE ===
-// WARNING! always comment out ONE of the two require() calls below.
-// 1. use next line to activate CUSTOM STYLE (./src/themes)
-// require('./themes/app.' + __THEME + '.styl')
-// 2. or, use next line to activate DEFAULT QUASAR STYLE
-require('quasar/dist/quasar.' + __THEME + '.css')
-// ==============================
 
 Quasar.theme.set(__THEME)
 Vue.use(Quasar) // Install Quasar Framework
@@ -19,7 +11,7 @@ Quasar.start(() => {
   /* eslint-disable no-new */
   new Vue({
     el: '#quasar-app',
-    router: Router,
+    router,
     render: h => h(App)
   })
 })
