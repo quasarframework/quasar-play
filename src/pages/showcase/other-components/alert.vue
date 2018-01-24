8e09902bf0c25bcf6577844e6fedcdcb6429aefc
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2018-01-24 19:26:51 +0200

    Update Alert page

--- src/pages/showcase/other-components/alert.vue
+++ src/pages/showcase/other-components/alert.vue
@@ -1,101 +1,108 @@
 <template>
-  <q-page padding>
-    <q-toggle v-model="visible" label="Show/hide next one" />
-    <br><br>
-    <transition
-      enter-active-class="animated bounceInLeft"
-      leave-active-class="animated bounceOutRight"
-      appear
-    >
-      <q-alert
-        v-if="visible"
-        color="brown"
-        icon="cloud"
-        style="margin-bottom: 1.5rem"
-      >
-        Lorem ipsum dolor sit amet.
+  <q-page padding class="row justify-center">
+    <div style="width: 500px; max-width: 90vw;">
+      <p class="caption">Simplest example, with a label only.</p>
+      <q-alert color="primary">With label only</q-alert>
+
+      <p class="caption">With label and icon</p>
+      <q-alert icon="map" color="orange">With label and icon</q-alert>
+
+      <p class="caption">With avatar</p>
+      <q-alert avatar="statics/boy-avatar.png" color="secondary">
+        With label and icon
       </q-alert>
-    </transition>
 
-    <q-toggle v-model="visible2" label="Show/hide next one" />
-    <br><br>
-
-    <transition
-      enter-active-class="animated flipInX"
-      leave-active-class="animated flipOutX"
-    >
+      <p class="caption">Using props, with subtitle.</p>
       <q-alert
-        v-if="visible2"
-        type="negative"
-        style="margin-bottom: 1.5rem"
-        :actions="[{label: 'Snooze', handler () {}}]"
+        avatar="statics/boy-avatar.png"
+        color="primary"
+        message="Jack"
+        detail="Your friend just pinged you"
+      />
+
+      <p class="caption">With Action(s)</p>
+      <q-alert
+        icon="alarm"
+        color="tertiary"
+        :actions="[
+          { label: 'Snooze', handler: () => { this.$q.notify('Snoozed') } }
+        ]"
+        class="q-mb-sm"
       >
-        Lorem ipsum dolor sit amet.
+        It's time to take a break
       </q-alert>
-    </transition>
+      <q-alert
+        icon="assignment"
+        color="brown-5"
+        :actions="[
+          { label: 'Snooze', icon: 'alarm', handler: () => { this.$q.notify('Snoozed') } },
+          { label: 'Dismiss', icon: 'done', handler: () => { this.$q.notify('Dismissed') } }
+        ]"
+        class="q-mb-sm"
+      >
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae ducimus voluptas recusandae numquam repellendus blanditiis aperiam unde. Mollitia sunt, ex distinctio eum debitis pariatur omnis ducimus molestiae optio quis eos.
+      </q-alert>
+      <q-alert
+        color="primary"
+        :actions="[
+          { label: 'OK', icon: 'watch_later', handler: () => { this.$q.notify('So please take that break') } }
+        ]"
+      >
+        It's time to take a break
+      </q-alert>
 
-    <br>
-    <q-alert icon="map" color="primary">Learn how you can get there</q-alert>
-    <br>
-    <q-alert
-      icon="map"
-      color="primary"
-      :actions="[ { label: 'Dismiss', handler: () => {} } ]"
-    >Learn how you can get there</q-alert>
-    <br>
-    <q-alert
-      icon="map"
-      color="primary"
-      :actions="[ { label: 'Reply', icon: 'map', handler: () => {} }, { label: 'Dismiss', handler: () => {} } ]"
-    >Learn how you can get there</q-alert>
-    <br>
-    <q-alert
-      icon="map"
-      color="primary"
-      :actions="[ { label: 'Reply', icon: 'map', handler: () => {} }, { label: 'Reply', icon: 'map', handler: () => {} }, { label: 'Dismiss', handler: () => {} } ]"
-    >Learn how you can get there</q-alert>
-    <br>
-    <q-alert
-      avatar="statics/boy-avatar.png"
-      color="dark"
-      :actions="[ { label: 'Reply', icon: 'map', handler: () => {} }, { label: 'Dismiss', handler: () => {} } ]"
-    >Your friend just pinged you</q-alert>
-    <br>
-    <q-alert
-      avatar="statics/boy-avatar.png"
-      color="amber"
-      text-color="black"
-      message="Title"
-      detail="Learn how you can get there"
-    />
-    <br>
-    <q-alert
-      avatar="statics/boy-avatar.png"
-      color="amber"
-      text-color="black"
-      :actions="[ { label: 'Dismiss', handler: () => {} } ]"
-    >
-      Your friend just pinged you
-    </q-alert>
-    <br>
+      <p class="caption">
+        Combining alerts with animations.
+        <q-btn
+          v-if="!visible || !visible2"
+          flat rounded
+          color="primary"
+          @click="() => { visible = visible2 = true }"
+          label="Reset"
+        />
+      </p>
 
-    <q-alert
-      v-for="type in ['positive', 'info', 'negative', 'warning']"
-      :key="type"
-      :type="type"
-      style="margin-bottom: 1.5rem"
-    >
-      Lorem ipsum dolor sit amet.
-    </q-alert>
+      <transition
+        enter-active-class="animated bounceInLeft"
+        leave-active-class="animated bounceOutRight"
+        appear
+      >
+        <q-alert
+          v-if="visible"
+          color="secondary"
+          icon="cloud"
+          appear
+          :actions="[{ label: 'Dismiss', handler: () => { visible = false } }]"
+          class="q-mb-sm"
+        >
+          Lorem ipsum dolor sit amet.
+        </q-alert>
+      </transition>
 
-    <q-alert
-      v-for="type in ['positive', 'info', 'negative', 'warning']"
-      :key="`sec${type}`"
-      :type="type"
-      style="margin-bottom: 1.5rem"
-    >
-      Non dismissible. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </q-alert>
+      <transition
+        enter-active-class="animated flipInX"
+        leave-active-class="animated flipOutX"
+        appear
+      >
+        <q-alert
+          v-if="visible2"
+          type="negative"
+          :actions="[{ label: 'Snooze', icon: 'alarm', handler: () => { visible2 = false } }]"
+        >
+          Lorem ipsum dolor sit amet.
+        </q-alert>
+      </transition>
+
+      <p class="caption">Default types of alerts</p>
+      <q-alert
+        v-for="type in ['positive', 'info', 'negative', 'warning']"
+        :key="type"
+        :type="type"
+        class="q-mb-sm"
+      >
+        Alert type: "{{ type }}"
+      </q-alert>
+    </div>
   </q-page>
 </template>
 
