6bd490c1c516ec92aae6caa461aa226fca6b3904
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2018-01-29 17:18:42 +0200

    List update

--- src/pages/showcase/grouping/list/chat.vue
+++ src/pages/showcase/grouping/list/chat.vue
@@ -26,6 +26,105 @@
           <q-item-main label="Jack Doe" />
         </q-item>
       </q-list>
+
+      <q-list inset-separator class="q-mt-md">
+        <q-item>
+          <q-item-side icon="voice_chat" color="secondary" />
+          <q-item-main label="Voice Chat with Joe" />
+          <q-item-side right icon="voice_chat" color="secondary" />
+        </q-item>
+        <q-item>
+          <q-item-side inverted icon="voice_chat" color="secondary" />
+          <q-item-main label="Voice Chat with Joe" />
+          <q-item-side right inverted icon="voice_chat" color="secondary" />
+        </q-item>
+        <q-item>
+          <q-item-side inverted icon="voice_chat" color="amber" text-color="black" />
+          <q-item-main label="Voice Chat with Joe" />
+          <q-item-side right inverted icon="voice_chat" color="amber" text-color="black" />
+        </q-item>
+        <q-item>
+          <q-item-side letter="J" color="secondary" />
+          <q-item-main label="John Doe" />
+          <q-item-side right letter="J" color="secondary" />
+        </q-item>
+        <q-item>
+          <q-item-side inverted letter="J" color="secondary" />
+          <q-item-main label="John Doe" />
+          <q-item-side right inverted letter="J" color="secondary" />
+        </q-item>
+        <q-item>
+          <q-item-side inverted letter="J" color="amber" text-color="black" />
+          <q-item-main label="John Doe" />
+          <q-item-side right inverted letter="J" color="amber" text-color="black" />
+        </q-item>
+      </q-list>
+
+      <q-list inset-separator class="q-mt-md">
+        <q-item>
+          <q-item-side>
+            <q-item-tile letter color="secondary">Q</q-item-tile>
+          </q-item-side>
+          <q-item-main label="John Doe" />
+          <q-item-side right>
+            <q-item-tile letter color="secondary">Q</q-item-tile>
+          </q-item-side>
+        </q-item>
+        <q-item>
+          <q-item-side>
+            <q-item-tile inverted letter color="secondary">Q</q-item-tile>
+          </q-item-side>
+          <q-item-main label="John Doe" />
+          <q-item-side right>
+            <q-item-tile inverted letter color="secondary">Q</q-item-tile>
+          </q-item-side>
+        </q-item>
+        <q-item>
+          <q-item-side>
+            <q-item-tile inverted letter color="amber" text-color="black">Q</q-item-tile>
+          </q-item-side>
+          <q-item-main label="John Doe" />
+          <q-item-side right>
+            <q-item-tile inverted letter color="amber" text-color="black">Q</q-item-tile>
+          </q-item-side>
+        </q-item>
+        <q-item>
+          <q-item-side>
+            <q-item-tile stamp color="secondary">Stamp</q-item-tile>
+          </q-item-side>
+          <q-item-main label="John Doe" />
+          <q-item-side right>
+            <q-item-tile stamp color="secondary">Stamp</q-item-tile>
+          </q-item-side>
+        </q-item>
+        <q-item>
+          <q-item-side>
+            <q-item-tile icon="map" color="secondary" />
+          </q-item-side>
+          <q-item-main label="John Doe" />
+          <q-item-side right>
+            <q-item-tile icon="map" color="secondary" />
+          </q-item-side>
+        </q-item>
+        <q-item>
+          <q-item-side>
+            <q-item-tile inverted icon="map" color="secondary" />
+          </q-item-side>
+          <q-item-main label="John Doe" />
+          <q-item-side right>
+            <q-item-tile inverted icon="map" color="secondary" />
+          </q-item-side>
+        </q-item>
+        <q-item>
+          <q-item-side>
+            <q-item-tile inverted icon="map" color="amber" text-color="black" />
+          </q-item-side>
+          <q-item-main label="John Doe" />
+          <q-item-side right>
+            <q-item-tile inverted icon="map" color="amber" text-color="black" />
+          </q-item-side>
+        </q-item>
+      </q-list>
     </div>
   </q-page>
 </template>
