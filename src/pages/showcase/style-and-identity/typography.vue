0d52d3a398b83ad2d54c060b96f43b0caa2f2b85
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2018-02-06 12:34:05 +0200

    Typography updates

--- src/pages/showcase/style-and-identity/typography.vue
+++ src/pages/showcase/style-and-identity/typography.vue
@@ -1,98 +1,64 @@
 <template>
   <q-page padding>
-    <q-card>
-      <q-card-title>
-        Headings
-      </q-card-title>
-      <q-card-separator />
-      <q-card-main>
-        <div v-for="heading in headings" :key="heading.class" class="row items-center q-mb-lg">
-          <div class="col-sm-3 col-12">
-            <q-chip color="primary" square>.{{ heading.class }}</q-chip>
-            <q-chip color="secondary" square v-if="heading.equivalent">{{ heading.equivalent }}</q-chip>
-          </div>
-          <div class="col-sm-9 col-12 q-pl-md q-pt-md">
-            <div class="q-mb-md" :class="[heading.class, `${heading.class}-opacity`]">{{ heading.label }}</div>
-            <div class="text-weight-light">
-              black <strong>{{ heading.color }}%</strong>, font weight <strong>{{ heading.weight }}</strong>
-            </div>
-          </div>
+    <p class="caption">Headings</p>
+    <div v-for="heading in headings" :key="heading.class" class="row items-center q-mb-lg">
+      <div class="col-sm-3 col-12">
+        <q-chip color="primary" square>.{{ heading.class }}</q-chip>
+        <q-chip color="secondary" square v-if="heading.equivalent">{{ heading.equivalent }}</q-chip>
+      </div>
+      <div class="col-sm-9 col-12 q-pl-md q-pt-md">
+        <div class="q-mb-md" :class="[heading.class, `${heading.class}-opacity`]">{{ heading.label }}</div>
+        <div class="text-weight-light">
+          black <strong>{{ heading.color }}%</strong>, font weight <strong>{{ heading.weight }}</strong>
         </div>
-      </q-card-main>
-    </q-card>
+      </div>
+    </div>
 
-    <q-card>
-      <q-card-title>
-        Weights
-      </q-card-title>
-      <q-card-separator />
-      <q-card-main>
-        <div v-for="weight in weights" :key="weight" class="row items-center q-mb-md">
-          <div class="col-sm-3 col-12">
-            <q-chip color="primary" square>.text-weight-{{ weight }}</q-chip>
-          </div>
-          <div class="col-sm-9 col-12 q-mb-none q-pl-md q-pt-sm q-pb-sm">
-            <div :class="`text-weight-${weight}`"> Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
-          </div>
-        </div>
-      </q-card-main>
-    </q-card>
+    <p class="caption">Weights</p>
+    <div v-for="weight in weights" :key="weight" class="row items-center q-mb-md">
+      <div class="col-sm-3 col-12">
+        <q-chip color="primary" square>.text-weight-{{ weight }}</q-chip>
+      </div>
+      <div class="col-sm-9 col-12 q-mb-none q-pl-md q-pt-sm q-pb-sm">
+        <div :class="`text-weight-${weight}`"> Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
+      </div>
+    </div>
 
-    <q-card>
-      <q-card-title>
-        Blockquotes
-      </q-card-title>
-      <q-card-main>
-        <blockquote>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-          <small>Someone famous for <cite title="Quasar Framework">Quasar Framework</cite></small>
-        </blockquote>
+    <p class="caption">Blockquotes</p>
+    <blockquote>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+      <small>Someone famous for <cite title="Quasar Framework">Quasar Framework</cite></small>
+    </blockquote>
 
-        <blockquote class="text-right">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-          <small>Someone famous for <cite title="Quasar Framework">Quasar Framework</cite></small>
-        </blockquote>
-      </q-card-main>
-    </q-card>
+    <blockquote class="text-right">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+      <small>Someone famous for <cite title="Quasar Framework">Quasar Framework</cite></small>
+    </blockquote>
 
-    <q-card>
-      <q-card-title>
-        Definition Lists
-      </q-card-title>
-      <q-card-main>
-        <p class="caption">Vertical</p>
+    <p class="caption">Definition Lists - Vertical</p>
+    <dl>
+      <dt>Description lists</dt>
+      <dd>A description list is perfect for defining terms.</dd>
+      <dt>Euismod</dt>
+      <dd>Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.</dd>
+      <dd>Donec id elit non mi porta gravida at eget metus.</dd>
+      <dt>Malesuada porta</dt>
+      <dd>Etiam porta sem malesuada magna mollis euismod.</dd>
+    </dl>
 
-        <dl>
-          <dt>Description lists</dt>
-          <dd>A description list is perfect for defining terms.</dd>
-          <dt>Euismod</dt>
-          <dd>Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.</dd>
-          <dd>Donec id elit non mi porta gravida at eget metus.</dd>
-          <dt>Malesuada porta</dt>
-          <dd>Etiam porta sem malesuada magna mollis euismod.</dd>
-        </dl>
+    <p class="caption">Definition Lists - Horizontal</p>
+    <dl class="horizontal">
+      <dt>Description lists</dt>
+      <dd>A description list is perfect for defining terms.</dd>
+      <dt>Euismod</dt>
+      <dd>Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.</dd>
+      <dd>Donec id elit non mi porta gravida at eget metus.</dd>
+      <dt>Malesuada porta</dt>
+      <dd>Etiam porta sem malesuada magna mollis euismod.</dd>
+    </dl>
 
-        <p class="caption">Horizontal</p>
-        <dl class="horizontal">
-          <dt>Description lists</dt>
-          <dd>A description list is perfect for defining terms.</dd>
-          <dt>Euismod</dt>
-          <dd>Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.</dd>
-          <dd>Donec id elit non mi porta gravida at eget metus.</dd>
-          <dt>Malesuada porta</dt>
-          <dd>Etiam porta sem malesuada magna mollis euismod.</dd>
-        </dl>
-      </q-card-main>
-    </q-card>
-
-    <q-card>
-      <q-card-title>
-        Links
-      </q-card-title>
-      <q-card-main>
-        <p>Links: <a>Some link</a> and <a>Some other link</a>.</p>
-      </q-card-main>
-    </q-card>
+    <p class="caption">Links</p>
+    <p>Links: <a>Some link</a> and <a>Some other link</a>.</p>
   </q-page>
 </template>
 
