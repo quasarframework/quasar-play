cbea242f038707a4975f7d907d95221b4edfe766
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2018-02-21 21:08:26 +0200

    Revert "add group with dropdown example (#39)"
    This reverts commit 7b8ba444dea4698568739cbf185ea072549dc9b3.

--- src/pages/showcase/scrolling/infinite-scroll.vue
+++ src/pages/showcase/scrolling/infinite-scroll.vue
@@ -12,8 +12,8 @@
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
         </p>
 
-        <div slot="message" class="row justify-center" style="margin-bottom: 50px;">
-          <q-spinner-dots :size="40" />
+        <div class="row justify-center" style="margin-bottom: 50px;">
+          <q-spinner-dots slot="message" :size="40" />
         </div>
       </q-infinite-scroll>
     </div>
