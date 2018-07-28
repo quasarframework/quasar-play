242ef4fd083bdc7fce2add5e0dd3b0fcb2a5aaa2
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2018-07-28 17:07:23 +0300

    chore: Bump version

--- quasar.conf.js
+++ quasar.conf.js
@@ -14,6 +14,7 @@ module.exports = function (ctx) {
     supportIE: true,
     build: {
       vueRouterMode: 'hash',
+      // analyze: true,
       extendWebpack (cfg) {
         cfg.module.rules.push({
           enforce: 'pre',
